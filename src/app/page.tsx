2e9d7ca4c6b82dae858587399d3d197fa5147ffe
drewcoble <thecoble@gmail.com>
2023-11-20 23:18:53 -0500

    lower case title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1>IsItFall.us</h1>
+        <h1>isitfall.us</h1>
         <h2>YES</h2>
       </div>
     </main>
