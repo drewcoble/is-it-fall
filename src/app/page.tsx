0710a74bce7217ae31e4676c1314f0ceb81d28dd
drewcoble <thecoble@gmail.com>
2023-11-21 20:52:49 -0500

    add fall hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import styles from "./page.module.css";
+import useFall from "./fall";
 
 export default function Home() {
+  const fall = useFall();
+  const fallString = fall ? "YES" : "NO";
   return (
     <main className={styles.main}>
       <div className={styles.center}>
         <h1>isitfall.us</h1>
-        <h2>YES</h2>
+        <h2>{fallString}</h2>
       </div>
     </main>
   );
